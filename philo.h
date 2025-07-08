e9d14c76ce77be5c940e1b9e0769df3d016b11cd
Lucas Bolens <lucasbolens@MacBook-Air-de-Lucas.local>
2025-07-08 12:42:20 +0200

    Added the header file

---
+++ philo.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <limits.h>
+
+typedef pthread_mutex_t mtx;
+
+typedef struct s_fork 
+{
+    mtx fork;
+    int fork_id;
+
+}   t_fork;
+
+typedef struct s_philo
+{
+    int philo_id;
+    long nbr_meals;
+    bool is_it_full;
+    long last_meal;
+    t_fork *left_fork;
+    t_fork *right_fork;
+    pthread_t thread_id;
+    t_table *table;
+}   t_philo;
+
+typedef struct s_table
+{
+    long nbr_philo;
+    long time_die;
+    long time_eat;
+    long time_sleep;
+    long max_meals;
+    long time_start;
+    long time_end;
+    t_fork *forks;
+    t_fork *philos;
+} t_table;
